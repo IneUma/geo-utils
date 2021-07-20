e5bf71721dda3f1b6cc81498fd3ea340f6a9f859
稲生悠 <accel.yutaka@gmail.com>
2021-07-20 18:41:38 +0900

    [clean] メソッドの変数名の調整

--- packages/vector/src/dot.ts
+++ packages/vector/src/dot.ts
@@ -4,15 +4,15 @@ import Vector from './Vector';
  * @name dot
  * @summary 2つのベクトルの内積を計算する
  *
- * @param {Vector} vec1 - ベクトル１
- * @param {Vector} vec2 - ベクトル2
+ * @param {Vector} vecA - ベクトル１
+ * @param {Vector} vecB - ベクトル2
  * @returns {number} 計算結果
  *
  * @example
  * const result = dot(new Vector(10, 20), new Vector(20, 10))
  */
-const dot = (vec1: Vector, vec2: Vector): number => {
-  return vec1.x * vec2.x + vec1.y * vec2.y;
+const dot = (vecA: Vector, vecB: Vector): number => {
+  return vecA.x * vecB.x + vecA.y * vecB.y;
 };
 
 export default dot;
--- packages/vector/src/sub.ts
+++ packages/vector/src/sub.ts
@@ -4,15 +4,15 @@ import Vector from './Vector';
  * @name sub
  * @summary 2つのベクトルを減算する
  *
- * @param {Vector} vec1 - ベクトル１
- * @param {Vector} vec2 - ベクトル2
+ * @param {Vector} vecA - ベクトル１
+ * @param {Vector} vecB - ベクトル2
  * @returns {Vector} 合成ベクトル
  *
  * @example
  * const result = sub(new Vector(10, 20), new Vector(20, 10))
  */
-const sub = (vec1: Vector, vec2: Vector): Vector => {
-  return new Vector(vec1.x - vec2.x, vec1.y - vec2.y);
+const sub = (vecA: Vector, vecB: Vector): Vector => {
+  return new Vector(vecA.x - vecB.x, vecA.y - vecB.y);
 };
 
 export default sub;
